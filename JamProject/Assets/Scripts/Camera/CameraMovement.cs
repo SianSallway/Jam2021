103881939d34e134f4466b4a10569b01c6dd2f52
SianSallway <37866416+SianSallway@users.noreply.github.com>
2021-01-29 21:15:31 +1100

    Adding project

---
+++ JamProject/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    Vector2 targetPos;
+    public Vector2 speed;
+    public float dampTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPos = target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.SmoothDamp(transform.position, targetPos, ref speed, dampTime);
+    }
+}
